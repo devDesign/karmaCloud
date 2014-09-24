f5d2dce05c9306179a2fab8d051f402c82ae05f5
Marlon <marlonkenny@gmail.com>
2014-09-24 23:56:01 +0000

    added KarmaGift model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :stories
+  has_many :gifted_karmas, class_name: 'KarmaGift', foreign_key: 'giver_id'
+  has_many :received_karmas, class_name: 'KarmaGift', foreign_key: 'receiver_id'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140924232346) do
+ActiveRecord::Schema.define(version: 20140924234120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20140924232346) do
     t.integer  "user_id"
     t.integer  "story_id"
     t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "karma_gifts", force: true do |t|
+    t.integer  "giver_id"
+    t.integer  "receiver_id"
+    t.integer  "amount"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
