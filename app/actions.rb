3bbcfeb020658baa7345d6f77f1b84f6e5389694
devDesign <marlyguha@gmail.com>
2014-09-24 15:46:02 -0700

    layout

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,21 @@ get "/" do
   slim :index, :layout => :layout
 end
 
+get "/user" do
+  slim :user, :layout => :layout
+end
+
+post "/user/new" do
+end
+
+get "/story" do
+  slim :, :layout => :layout
+end
+
+post "/story/new" do
+end
+
+get "/" do
+  slim :index, :layout => :layout
+end
+
