966d71e944c74c3b8c2f2b9b53296a6a683fd527
deereb <brendan.g.deere@gmail.com>
2014-09-25 14:17:15 -0700

    database seed and test db

--- app/models/story.rb
+++ app/models/story.rb
@@ -5,6 +5,5 @@ class Story < ActiveRecord::Base
   validates :title, :content, :user_id, presence: true
   validates :mood, format: { with: /\Ared|green\Z/, message: "must be 'red' or 'green'"}
 
-  geocoded_by      :location
-  after_validation :geocode
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,30 +2,44 @@
 FactoryGirl.define do
   factory :user do
     user_name    Faker::Internet.user_name
-    password     Faker::Internet.password(10, 20)
+    password     "test"
     name         Faker::Name.name
     email        Faker::Internet.email
     latitude     Faker::Address.latitude
     longitude    Faker::Address.longitude
-    karma_total  rand(100)
-    karma_bank   rand(500_000)
+    karma_total  100
+    karma_bank   10000
+
+    factory :user_with_story do
+      
+      after(:create ) do |user, evaluator|
+        create_list(:story_with_comments, 5, user: user)
+      end
+    end
   end
 
+  factory :story do
+    title        Faker::Lorem.sentence
+    content      Faker::Lorem.paragraph
+    latitude     Faker::Address.latitude
+    longitude    Faker::Address.longitude
+    mood         "green"  
+    user
 
-trait :with_stories do
-  
-  after :create do |user|
-    FactoryGirl.create_list :story 
+    factory :story_with_comments do 
+      after(:create) do |story, evaluator|
+        create_list(:comment, 5, story: story, user: story.user)
+      end
+    end
+  end
+
+  factory :comment do
+    user
+    story 
+    content Faker::Lorem.sentence
   end
 end
 
 
 
-# factory :story do
-#       title        Faker::Lorem.sentence(3)
-#       content      Faker::Lorem.paragraph
-#       latitude     Faker::Address.latitude
-#       longitude    Faker::Address.longitude
-#       mood         # green or red
-#       user    
-#     end
+user = FactoryGirl.create :user_with_story
