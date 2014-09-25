ef2de7316d7a01d751edbf22a8f835882b0a166d
deereb <brendan.g.deere@gmail.com>
2014-09-25 11:58:15 -0700

    Revert "revert me"
    This reverts commit 439ecf20423363a8cdb5d0d7ac8fb496d6c254db.

--- config/database.rb
+++ config/database.rb
@@ -2,26 +2,19 @@ configure do
   # Log queries to STDOUT in development
   if Sinatra::Application.development?
     ActiveRecord::Base.logger = Logger.new(STDOUT)
-    
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
-  else
-
-    ActiveRecord::Base.establish_connection(
-      adapter: 'postgresql',
-      encoding: 'unicode',
-      pool: 5,
-      database: 'd2tc9g7mqg58ls',
-      username: 'fdupvvlrphtfgn',
-      password: 'Uu-jSJyAQu4bKuQ32JoiPcN5eG',
-      host: 'ec2-54-83-204-85.compute-1.amazonaws.com',
-      port: 5432,
-      min_messages: 'error'
-    )
-
   end
+
+  ActiveRecord::Base.establish_connection(
+    adapter: 'postgresql',
+    encoding: 'unicode',
+    pool: 5,
+    database: 'd2tc9g7mqg58ls',
+    username: 'fdupvvlrphtfgn',
+    password: 'Uu-jSJyAQu4bKuQ32JoiPcN5eG',
+    host: 'ec2-54-83-204-85.compute-1.amazonaws.com',
+    port: 5432,
+    min_messages: 'error'
+  )
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
