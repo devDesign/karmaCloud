f221b5da03c31fe416de72c1f8a2a45cadd9bc24
devDesign <marlyguha@gmail.com>
2014-09-25 19:22:09 -0700

    layout merge

--- app/actions.rb
+++ app/actions.rb
@@ -25,53 +25,8 @@ helpers do
   end
 end
 
-<<<<<<< HEAD
-=======
 get "/" do
-
-  gon.stories = [
-    {
-      latitude: 49.281956099999995,
-      longitude: -123.108346,
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien vel mauris aliquet commodo. Pellentesque non lacus ante. Nunc nibh lectus, dapibus ut fringilla sit amet, lacinia sit amet magna. Nam rhoncus lectus eu nunc efficitur viverra. Phasellus pellentesque eget orci vel ultricies. Phasellus in odio in tellus fermentum hendrerit sed a ante. Cras dignissim, ex gravida vestibulum finibus, odio justo condimentum libero, id finibus mi elit sit amet enim. Quisque vulputate viverra eros, at viverra nibh pretium ac. Duis molestie ante vitae elit pellentesque, at fringilla nisi congue. Pellentesque sagittis rhoncus pellentesque. Vivamus scelerisque, mauris vitae interdum fermentum, orci massa pharetra massa, ultricies efficitur odio arcu eu sem. Nam suscipit nulla sed ipsum lacinia, eu imperdiet ante condimentum. Proin accumsan cursus ligula, vel luctus libero lacinia sit amet.",
-      posted_by: "some user"
-    },
-
-    {
-      latitude: 50.2819561,
-      longitude: -123.1083461,
-      content: "Story content",
-      posted_by: "some user"
-    },
-
-    {
-      latitude: 49.2819562,
-      longitude: -123.1083462,
-      content: "Story content",
-      posted_by: "some user"
-    },
-
-    {
-      latitude: 49.2819563,
-      longitude: -124.1083463,
-      content: "Story content",
-      posted_by: "some user"
-    },
-
-    {
-      latitude: 49.63,
-      longitude: -123.1083464,
-      content: "Story content",
-      posted_by: "some user"
-    },
-
-    {
-      latitude: 49.2819563,
-      longitude: -123.108,
-      content: "Story content",
-      posted_by: "some user"
-    }
-  ]
+  
   @user = User.new
   @users = User.all
   @stories = Story.all
@@ -79,7 +34,7 @@ get "/" do
 
 end
 
->>>>>>> 7bff50bb101ba2083e39e27a0e78c7b28d60a7e0
+
 post "/user_session/new" do
   @user = User.where(user_name: params[:user_name]).first || User.new
   if login_valid?
